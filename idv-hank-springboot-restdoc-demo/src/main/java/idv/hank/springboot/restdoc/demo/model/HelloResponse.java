1f8586b74eb5dfdfd74f79048371974aa4d77044
Po-Han Hao <pohan.hao@gmail.com>
2016-08-18 22:30:30 +0800

    [ADD] add spring restdoc module.

---
+++ idv-hank-springboot-restdoc-demo/src/main/java/idv/hank/springboot/restdoc/demo/model/HelloResponse.java
@@ -0,0 +1,14 @@
+package idv.hank.springboot.restdoc.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class HelloResponse {
+
+    private String name;
+
+    private String message;
+
+}
